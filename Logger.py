1f38aa90bd9d7785e980da5cd8f5c0ea2fe5cdff
JannesSP <janspang@gmx.de>
2022-07-05 14:32:06 +0200

    update

--- Logger.py
+++ Logger.py
@@ -14,43 +14,61 @@ class Logger():
     def __init__(self, logfilepointer: TextIOWrapper = None):
         self.lp = logfilepointer
 
-    def writeLog(self, string):
+    def writeLog(self, string : str, flush : bool = False) -> None:
         '''
         Write string to logfile if logfilepointer is set.
-        
-        @param string: String to write to logfile.
+
+        Parameters
+        ----------
+        string : str
+            Message to write to logfile
         '''
         if self.lp is not None:
             self.lp.write(string)
 
-    def error(self, string, error_type=1):
+            if flush:
+                self.lp.flush()
+
+    def error(self, string : str, error_type : int = 1) -> None:
         '''
         Write error string to stderr and logfile if logfilepointer is set.
         Exit program with given error code.
         
-        @param string: String to write to stderr and logfile.
-        @param error_type: used error_type, default 1.
+        Parameters
+        ----------
+        string : str
+            Message to write to stderr and logfile
+        error_type : int
+            used error_type, default 1
         '''
         sys.stderr.write(f'ERROR: {string}\n')
         self.writeLog(f'ERROR: {string}\n')
         sys.exit(error_type)
 
-    def warning(self, string):
+    def warning(self, string : str) -> None:
         '''
         Write warning string to stderr and logfile if logfilepointer is set.
         
-        @param string: String to write to stderr and logfile.
+        Parameters
+        ----------
+        string : str
+            Message to write to stderr and logfile
         '''
         sys.stderr.write(f'WARNING: {string}\n')
         self.writeLog(f'WARNING: {string}\n')
 
-    def printLog(self, string, newline_before=False, newline_after=True):
+    def printLog(self, string : str, newline_before : bool = False, newline_after : bool = True) -> None:
         '''
         Write datetime and string to stdout and logfile if logfilepointer is set.
         
-        @param string: String to write to stdout and logfile.
-        @param newline_before: Add newline before string, default False.
-        @param newline_after: Add newline after string, default True.
+        Parameters
+        ----------
+        string : str
+            Message to write to stdout and logfile.
+        newline_before : bool
+            Add newline before string, default False.
+        newline_after : bool
+            Add newline after string, default True.
         '''
         if newline_before:
             sys.stdout.write('\n')
@@ -62,3 +80,10 @@ class Logger():
         if newline_after:
             sys.stdout.write('\n')
             self.writeLog('\n')
+
+    def close(self) -> None:
+        '''
+        Close TextIOWrapper
+        '''
+        if self.lp is not None:
+            self.lp.close()
